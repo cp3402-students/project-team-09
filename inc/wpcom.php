2f4211f19120c6d358a720d6bf6c9715609da03b
James McNeill <james.mcneill@my.jcu.edu.au>
2023-04-29 11:33:57 +1000

    performed find and replace procedure in _s quickstart

--- inc/wpcom.php
+++ inc/wpcom.php
@@ -4,7 +4,7 @@
  *
  * This file is centrally included from `wp-content/mu-plugins/wpcom-theme-compat.php`.
  *
- * @package _s
+ * @packageTsvCountryMusic
  */
 
 /**
@@ -12,7 +12,7 @@
  *
  * @global array $themecolors
  */
-function _s_wpcom_setup() {
+function tsvcountrymusic_wpcom_setup() {
 	global $themecolors;
 
 	// Set theme colors for third party services.
@@ -28,4 +28,4 @@ function _s_wpcom_setup() {
 		);
 	}
 }
-add_action( 'after_setup_theme', '_s_wpcom_setup' );
+add_action( 'after_setup_theme', 'tsvcountrymusic_wpcom_setup' );
--- page.php
+++ page.php
@@ -9,7 +9,7 @@
  *
  * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
  *
- * @package _s
+ * @packageTsvCountryMusic
  */
 
 get_header();
