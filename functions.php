114cb0a46d1334535c8fd37599698b9cf7613933
James McNeill <james.mcneill@my.jcu.edu.au>
2023-05-21 12:03:12 +1000

    initial footerRefactor (incomplete)

--- functions.php
+++ functions.php
@@ -133,6 +133,17 @@ function tsvcountrymusic_widgets_init() {
 			'after_title'   => '</h2>',
 		)
 	);
+	register_sidebar(
+		array(
+			'name'          => esc_html__( 'Footer', 'tsvcountrymusic' ),
+			'id'            => 'footer-1',
+			'description'   => esc_html__( 'Add widgets here.', 'tsvcountrymusic' ),
+			'before_widget' => '<section id="%1$s" class="widget contacts-widget %2$s">',
+			'after_widget'  => '</section>',
+			'before_title'  => '<h2 class="widget-title">',
+			'after_title'   => '</h2>',
+		)
+	);
 }
 
 add_action( 'widgets_init', 'tsvcountrymusic_widgets_init' );
